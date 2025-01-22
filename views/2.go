ff44c927ec7334b58040d67dfafe75d109eaa78d
luqmanzakariya <luqman.zakariya@gmail.com>
2025-01-23 05:32:38 +0700

    add fetch data customer before order

--- views/2.go
+++ views/2.go
@@ -1,8 +1,13 @@
 package views
 
 import (
+	"beverages-cli/handler"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func View2() {
@@ -12,13 +17,39 @@ func View2() {
 		fmt.Println("=======================================")
 		fmt.Println("Order")
 		fmt.Println("=======================================")
-		fmt.Println("*input 0 to exit")
 		fmt.Println("")
 
+		// # Fetch data customers
+		customers, err := handler.GetAllCustomers()
+		if err != nil {
+			log.Fatal("Failed to retrieve data from database: ", err)
+		}
+
+		// # Create a new table writer
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "USERNAME", "NAME", "EMAIL", "PHONE", "ADDRESS"}) // Table headers
+
+		// # Add rows to the table
+		for _, value := range customers {
+			row := []string{
+				strconv.Itoa(value.UserID),
+				value.Username,
+				value.Name,
+				value.Email,
+				value.PhoneNumber,
+				value.Address,
+			}
+			table.Append(row)
+		}
+
+		// # Render the response and table
+		table.Render()
+
 		// # Waiting input Customer ID
-		fmt.Print("Please choose customer number: ")
+		fmt.Println("")
+		fmt.Print("Choose registered customer ID to order (0 to exit): ")
 		var customerId string
-		_, err := fmt.Scan(&customerId)
+		_, err = fmt.Scan(&customerId)
 		if err != nil {
 			log.Fatal("Failed to read customerId")
 		}
