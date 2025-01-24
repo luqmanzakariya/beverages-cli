ff709f271537b981e78cb47c9dc576d303d168b8
luqmanzakariya <luqman.zakariya@gmail.com>
2025-01-24 16:37:23 +0700

    ==== FIX GAK ADA TAMPILAN LIST PRODUCTS

--- views/3.go
+++ views/3.go
@@ -1,8 +1,13 @@
 package views
 
 import (
+	"beverages-cli/handler"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func View3() {
@@ -13,6 +18,31 @@ func View3() {
 		fmt.Println("List Product & Stock")
 		fmt.Println("=======================================")
 		fmt.Println("")
+		// # Fetch data products
+		products, err := handler.GetListProducts()
+		if err != nil {
+			log.Fatal("Failed to retrieve data from database: ", err)
+		}
+
+		// # Create a new table writer
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "PRODUCT NAME", "PRICE", "AVAILABLE", "CATEGORY"}) // Table headers
+
+		// # Add rows to the table
+		for _, value := range products {
+			row := []string{
+				strconv.Itoa(value.ProductID),
+				value.ProductName,
+				fmt.Sprintf("%.2f", value.Price),
+				strconv.Itoa(value.StockQuantity),
+				value.CategoryName,
+			}
+			table.Append(row)
+		}
+
+		// # Render the response and table
+		table.Render()
+		fmt.Println("")
 		fmt.Println("1. Add new product")
 		fmt.Println("2. Edit product")
 		fmt.Println("3. Delete product")
@@ -21,7 +51,7 @@ func View3() {
 
 		// # Waiting user input
 		var userInput string
-		_, err := fmt.Scan(&userInput)
+		_, err = fmt.Scan(&userInput)
 		if err != nil {
 			log.Fatal("Failed to read userInput")
 		}
